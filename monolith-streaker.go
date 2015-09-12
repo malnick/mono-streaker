5ab4863506843f0186de3630d787f85a6a2f299d
malnick <malnick@gmail..com>
2015-09-12 16:50:09 -0700

    mono streaker

---
+++ monolith-streaker.go
@@ -0,0 +1,27 @@
+// +build linux darwin amd64
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func Streaker(w http.ResponseWriter, req *http.Request) {
+	index, _ := ioutil.ReadFile("./monolith.html")
+	io.WriteString(w, string(index))
+}
+
+func main() {
+	log.Info("STREAKER ON :8000!")
+	//Handle request
+	http.HandleFunc("/", Streaker)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Error("Can't start streaker!")
+		log.Error(err)
+		os.Exit(1)
+	}
+}
